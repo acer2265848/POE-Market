ca8e96778cb31d2fd87047836e5417433624e215
Rod <a2265848@gmail.com>
2019-12-16 19:58:01 +0800

    version  revised

--- poe_market.py
+++ poe_market.py
@@ -10,10 +10,14 @@ import pandas as pd
 import math
 import re
 import numpy as np
+from scipy.stats import iqr
+from operator import itemgetter
+#%%
 league_name="凋落聯盟"
 # =============================================================================
 # 
 # =============================================================================
+#%%
 from google.cloud import bigquery as bq
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./dashboardmap-800d89b22555.json"
@@ -35,7 +39,7 @@ table = bq.Table(table_id, schema=tschema)
 table = client.create_table(table)  # API request
 print(f"Created dataset {client.project}.{dataset.dataset_id}")
 print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
-
+#%%
 
 
 
@@ -43,45 +47,45 @@ print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
 # currency
 # =============================================================================
 
-
+#%%
 #query curr, use F12 to find query
 query_curr1 = {"exchange":{"status":{"option":"online"},"have":["chaos"],"want":["exa"]}}
 #convert dic to json str
-query_curr1=json.dumps(query_curr1)
+query_curr1= json.dumps(query_curr1)
 query_curr1_ID = requests.get(url="http://web.poe.garena.tw/api/trade/exchange/"+league_name+"?source=" +(query_curr1))
 #query_curr1_ID = requests.post(url="http://web.poe.garena.tw/api/trade/exchange/%E6%88%B0%E4%BA%82%E8%81%AF%E7%9B%9F", data= query_curr1)
 query_curr1_list = json.loads(query_curr1_ID.text)
 #get the number
 stop_num = math.ceil(query_curr1_list['total']/10) 
-
-query_curr1_all = []
+#每10個依序讀取
+CurList = []
 for i in range(0,stop_num):
     query_curr1_str = ','.join(query_curr1_list['result'][i*10:(i+1)*10])
     query_curr1_detail = requests.get("http://web.poe.garena.tw/api/trade/fetch" +"/"+ query_curr1_str +"?query=elJEbsL&exchange").json()
 #    print(i)
-    query_curr1_all.extend(query_curr1_detail['result'])
+    CurList.extend(query_curr1_detail['result'])
 
-for i in range(len(query_curr1_all)):
-    query_curr1_all[i]['listing']['price']['exchange']['amount']
-# RE找出數字 → list → str → int
-#curr_list = [','.join(re.findall(r"(\d+)",query_curr1_all[i]['item']['note'])) for i in range(len(query_curr1_all))]
-#找出有多少各ID數量可交易
-curr_list = [query_curr1_all[i]['listing']['price']['exchange']['amount'] \
-             for i in range(len(query_curr1_all))]
+
+#找出各ID交易需求
+CurrAmount = np.array([CurList[i]['listing']['price']['exchange']['amount'] \
+             for i in range(len(CurList))])
 # int 2 str, replace ',' to '.', str 2 float, float 2 round2
-curr_list = [round(float(str(w).replace(',', '.')),2) for w in curr_list]
-
-for i in range(len(query_curr1_all)):
-    query_curr1_all[i]['listing']['price']['item']['stock']
-
-curr_list = [query_curr1_all[i]['listing']['price']['item']['stock'] \
-             for i in range(len(query_curr1_all))]
+CurrAmount = [round(float(str(w).replace(',', '.')),2) for w in CurrAmount]
 
 
-exa2c = np.median(curr_list)
-np.mean(curr_list)
+#找出有多少各ID數量可交易
+CurrStock = [CurList[i]['listing']['price']['item']['stock'] \
+             for i in range(len(CurList))]
+MinInterval = np.quantile(CurrAmount, .25) -1.5*iqr(CurrAmount)
+MaxInterval = np.quantile(CurrAmount, .75) +1.5*iqr(CurrAmount)
 
+NoOutlier = [ [i,x] for i,x in enumerate(CurrAmount) if x>= MinInterval and x<= MaxInterval]
+minpic = min(NoOutlier,key=itemgetter(1))
 
+exa2c = np.median(CurrStock)
+np.mean(CurrStock)
+
+#%%
 # =============================================================================
 # item
 # =============================================================================
